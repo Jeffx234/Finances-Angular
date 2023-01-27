f288d3e94094bad7077bc95d2c405ce56a9429cc
Jeffx234 <jefersonluisx@hotmail.com>
2023-01-27 18:05:38 -0300

    feat(styles): adding styles category list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoriesModule } from './pages/categories/categories.module';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+  ],
+  exports: [
+    CategoriesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from "@angular/router";
 
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryListComponent } from './category-list/category-list.component';
@@ -9,11 +10,12 @@ import { CategoryFormComponent } from './category-form/category-form.component';
 @NgModule({
   declarations: [
     CategoryListComponent,
-    CategoryFormComponent
+    CategoryFormComponent,
   ],
   imports: [
     CommonModule,
-    CategoriesRoutingModule
+    CategoriesRoutingModule,
+    RouterModule
   ]
 })
 export class CategoriesModule { }
